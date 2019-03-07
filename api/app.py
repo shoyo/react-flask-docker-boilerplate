dbcef2c37ae9f3e14fc43fed582fc5e151b8654b
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-03-07 20:42:23 +0900

    Add comments for readability

--- api/app.py
+++ api/app.py
@@ -4,15 +4,22 @@ app = Flask(__name__)
 
 
 @app.route('/api/v1.0/test', methods=['GET'])
-def testing():
-    test = {
+def test_response():
+    """Return a sample JSON response."""
+    sample_response = {
         "items": [
             { "id": 1, "name": "Apples",  "price": "$2" },
             { "id": 2, "name": "Peaches", "price": "$5" }
         ]
     }
-    # test = 'This is the API response.'
-    response = make_response(jsonify(test))
+    # JSONify response
+    response = make_response(jsonify(sample_response))
 
+    # Add Access-Control-Allow-Origin header to allow cross-site request
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+
+    # Mozilla provides good references for Access Control at:
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Server-Side_Access_Control
+
     return response
