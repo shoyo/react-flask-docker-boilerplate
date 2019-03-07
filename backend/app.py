f4299a3ca4cc62a37743a86346d0293780d36a53
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-03-07 11:47:49 +0900

    Configure dummy backend API

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!!!'
+
+
+@app.route('/api/v1.0/test', methods=['GET'])
+def testing():
+    test = {
+        "items": [
+            { "id": 1, "name": "Apples",  "price": "$2" },
+            { "id": 2, "name": "Peaches", "price": "$5" }
+        ]
+    }
+    # test = 'This is the API response.'
+    response = make_response(jsonify(test))
+
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
